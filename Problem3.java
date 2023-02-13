5de33e44cfd85f213bc39fe0a7fb2b78ccd94d30
karthick Raja <89631459+Karthickraja1909@users.noreply.github.com>
2023-02-13 21:06:22 +0530

    Add files via upload

---
+++ Problem3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       String myString = scanner.next();
+       int myInt = scanner.nextInt();
+              int myIntb = scanner.nextInt();
+
+       scanner.close();
+
+       System.out.println(myString);
+       System.out.println(myInt);
+       System.out.println(myIntb);
+
+    }
+}
